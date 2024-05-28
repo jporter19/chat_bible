6130cf873bc7cd479913adf98cb85db8bb38f5d0
John Porter <jporter19@duck.com>
2024-05-28 16:01:39 -0400

    first commit - committing all files to repo

---
+++ lc-beautiful_soup.py
@@ -0,0 +1,16 @@
+
+from pprint import pprint
+from langchain_community.document_loaders import AsyncChromiumLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
+
+# Load HTML
+# loader = AsyncChromiumLoader(["https://www.wsj.com"])
+loader = AsyncChromiumLoader(["https://www.newadvent.org"])
+html = loader.load()
+# print(html)
+
+bs_transformer = BeautifulSoupTransformer()
+docs_transformed = bs_transformer.transform_documents(
+    html, tags_to_extract=["p", "li", "div", "a"]
+)
+pprint(docs_transformed[0].page_content[0:4000])
