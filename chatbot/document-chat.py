ed9e2c9c2ba5ac5c8ea1e5bbdea58861be895a9f
John <jnjporter19@gmail.com>
2024-06-02 16:13:48 -0400

    using a langchain chroma similarity search. changed the display of the text show only text and metadata getting rid of object data.  cleaned up some unused code.

--- chatbot/document-chat.py
+++ chatbot/document-chat.py
@@ -8,9 +8,12 @@ from chromadb.config import Settings
 from tqdm.autonotebook import tqdm, trange
 from langchain_community.embeddings.sentence_transformer import (SentenceTransformerEmbeddings,)
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-import re    # re is regular expressions for pattern matching
+# from pypdf import PdfReader, PdfWriter  # reads pdf files
+import pymupdf
+import pdfplumber
+from io import StringIO
+import re   # re is regular expressions for pattern matching
 #default_ef = embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-#default_ef = embedding_functions.DefaultEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
 
 #%% - this is a path to the primary database, each database can have multiple collections
@@ -26,20 +29,18 @@ def my_collections(db):
         collections.append(db.list_collections()[i].name)
     return collections
 
-#%%
+#%%  ------------------  Open an Existing Chroma DB as a Client  -----------------------------
 run_client = chromadb.PersistentClient(path=dbpath)
+# --------------------  This is a side bar collection pickter  ----------------------------
 st.session_state.col_choice = st.sidebar.selectbox("Pick a Collection",my_collections(run_client))
+#%%  ---------If a Collection is selected Create a LangChain Chroma instance  ------------------
 if st.session_state.col_choice:
     db= Chroma(persist_directory=dbpath,collection_name=st.session_state.col_choice,embedding_function=embedding_function)
 resp_cnt = st.sidebar.slider(min_value=1,max_value=10,label="Set the Number of Response")
-# st.write(lc_client._collection.count())
-# chroma_collection = chroma_db.get_collection(col_nm)  # Create a collection
-# pprint(chroma_db.list_collections()) # print all collections in database
-# ***** chroma_db.delete_collection("bluemetal")
-#  This code will produce a list of collection names in the DB
+########### - Testing - ###########
+# st.write(db._collection.count())
 
-
-#%%
+#%% 
 def closest_index(dists, val):
     # --------------- the absolute value of subtracting 1 from every number above and below 1, the closest will be the smallest
     difference = [abs(dist-1) for dist in dists]
@@ -65,19 +66,19 @@ with tab1:
         st.subheader(f'Collection: [{st.session_state.col_choice}]',divider=True)
         prompt=(st.chat_input('What is your question?'))
         if prompt: # if prompt has data then run this
-            ##%%
-            st.write(prompt)
-            res = db.similarity_search(prompt)
-            st.write(res)
-           # res = chroma_collection.query(query_texts=prompt, 
-           #     n_results=18,
-           #     include=["distances","documents","embeddings","metadatas"])
+            res = db.similarity_search_with_relevance_scores(prompt,resp_cnt)
+            resp_cnt = len(res)
+            for doc in res:
+                # st.write(doc)
+                st.write(doc[0].page_content)
+                st.write(f'Source: {doc[0].metadata['source']}')
+                st.button('See More',key=doc.index)
+
             #docs = closest_index(res["distances"][0],resp_cnt)
             #best_docs = ''
             #for d in docs:
             #    best_docs = best_docs + res["documents"][0][d] +  f'  \n :blue[Source:] *{res["metadatas"][0][d].get('source')}*' +'\n\n'
-                # best_docs = best_docs + res["documents"][0][d] +  '  '  + 'www.newadvent.org'
-                
+                # best_docs = best_docs + res["documents"][0][d] +  '  '  + 'www.newadvent.org'   
             # st.write(best_docs)
             prompt = prompt + ' -- Collection  \{'+st.session_state.col_choice+'\}'
             #st.session_state.prompt_res.update({prompt:best_docs[0]})
@@ -89,8 +90,17 @@ with tab1:
                     message.write(key)
                     mycont=st.container(height=250, border=True)
                     with mycont: st.write(st.session_state.prompt_res[key])
-        
+with tab2:
+    #import 
+    #%%
+    from langchain_community.document_loaders import TextLoader
 
-
-
-# %%
+    #%%
+    uploaded_file = st.file_uploader(':blue[Choose a File]',type=['txt','pdf'],accept_multiple_files=False,label_visibility='hidden')
+    #uploaded_files = st.file_uploader(':blue[Choose a File]',type=['txt','pdf'],accept_multiple_files=True,label_visibility='hidden')
+    if uploaded_file is not None:
+        st.success("Uploaded the file")
+        with pdfplumber.open(uploaded_file) as file:
+            all_pages = file.pages
+            st.write(len(all_pages))
+            st.write(all_pages[0].extract_text()) # you can print and check the data from any page in pdf   
